3c4bb1ed733f7004cc24809af445fedfa8804caa
Ninu <ninuvv032@gmail.com>
2020-12-06 20:45:53 +0300

    otp module not completed failed

--- routes/student.js
+++ routes/student.js
@@ -8,9 +8,11 @@ const Nexmo = require('nexmo');
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
 nunjucks.configure('views', { express: app })
+var unirest = require('unirest');
+var request = require('request')
 
 require('dotenv').config()
-
+// const TOKEN=process.env.token
 const nexmo = new Nexmo({
   apiKey: '6de2e27f',
   apiSecret: 'zL1PccyARFVhLaDB',
@@ -66,65 +68,95 @@ router.get('/s_forgetPassword', (req, res) => {
   res.render('student/s_forgetPassword')
 })
 
+const TOKEN=process.env.token
 
-
-
-var request = require('request');
-var VERSION = "0.1";
-
-
-//https://www.twilio.com/docs/verify/api/v1/quickstart/nodejs?code-sample=code-send-a-phone-verification-via-sms-or-voice-2&code-language=Node.js&code-sdk-version=default
-module.exports = function (apiKey, apiUrl) {
-  return new PhoneVerification(apiKey, apiUrl);
-};
-
-function PhoneVerification(apiKey, apiUrl) {
-  this.apiKey = AC8646e238d2918c23b7696f4d33fa8625;
-  this.apiURL = apiUrl || "https://api.authy.com";
-  this.user_agent = "PhoneVerificationRegNode/" + VERSION + " (node " + process.version + ")";
-  this.headers = {};
-
-  this.init();
-}
-
-PhoneVerification.prototype.init = function () {
-  this.headers = {
-      "User-Agent": this.user_agent
-  };
-};
+// console.log(${TOKEN})
 
 router.post('/s_verifyPassword', function (req, res) {
-  phone_number=req.body.mob,
-  country_code=req.body.code
-  // country_code='+966',
-  via="SMS"
-  PhoneVerification.prototype.requestPhoneVerification = function (phone_number, country_code, via, callback) {
-console.log("verify")
-    this._request("post", "/protected/json/phones/verification/start", {
-            "api_key": this.apiKey,
-            "phone_number": phone_number,
-            "via": via,
-            "country_code": country_code,
-            "code_length": 4
-        },
-        callback
-    );
-};
+  
+  var options = {
+    'method': 'POST',
+    'url': 'https://d7networks.com/api/verifier/send',
+    'headers': {
+      'Authorization': 'Token cnJubjMxOTA6eGFidFpCS08'
+    },
+    formData: {
+      'mobile': '966'+req.body.mob,
+      'sender_id': 'SMSINFO',
+      'message': 'Your otp code is {code}',
+      'expiry': '900'
+    }
+  };
+  request(options, function (error, response) {
+    if (error) throw new Error(error);
+    console.log(response.body);
+  });
+
 })
 
 router.post('/s_checkOTP', function (req, res) {
-  PhoneVerification.prototype.verifyPhoneToken = function (phone_number, country_code, token, callback) {
 
-    console.log('in verify phone');
-    this._request("get", "/protected/json/phones/verification/check", {
-            "api_key": this.apiKey,
-            "verification_code": token,
-            "phone_number": phone_number,
-            "country_code": country_code
-        },
-        callback
-    );
-};
+})
+
+
+
+
+// var request = require('request');
+// var VERSION = "0.1";
+
+// // https://github.com/TwilioDevEd/account-security-quickstart-node/blob/master/server/lib/phone_verification.js
+// //https://www.twilio.com/docs/verify/api/v1/quickstart/nodejs?code-sample=code-send-a-phone-verification-via-sms-or-voice-2&code-language=Node.js&code-sdk-version=default
+// module.exports = function (apiKey, apiUrl) {
+//   return new PhoneVerification(apiKey, apiUrl);
+// };
+
+// function PhoneVerification(apiKey, apiUrl) {
+//   this.apiKey = AC8646e238d2918c23b7696f4d33fa8625;
+//   this.apiURL = apiUrl || "https://api.authy.com";
+//   this.user_agent = "PhoneVerificationRegNode/" + VERSION + " (node " + process.version + ")";
+//   this.headers = {};
+
+//   this.init();
+// }
+
+// PhoneVerification.prototype.init = function () {
+//   this.headers = {
+//       "User-Agent": this.user_agent
+//   };
+// };
+
+// router.post('/s_verifyPassword', function (req, res) {
+//   phone_number=req.body.mob,
+//   country_code=req.body.code
+//   // country_code='+966',
+//   via="SMS"
+//   PhoneVerification.prototype.requestPhoneVerification = function (phone_number, country_code, via, callback) {
+// console.log("verify")
+//     this._request("post", "/protected/json/phones/verification/start", {
+//             "api_key": this.apiKey,
+//             "phone_number": phone_number,
+//             "via": via,
+//             "country_code": country_code,
+//             "code_length": 4
+//         },
+//         callback
+//     );
+// };
+// })
+
+// router.post('/s_checkOTP', function (req, res) {
+//   PhoneVerification.prototype.verifyPhoneToken = function (phone_number, country_code, token, callback) {
+
+//     console.log('in verify phone');
+//     this._request("get", "/protected/json/phones/verification/check", {
+//             "api_key": this.apiKey,
+//             "verification_code": token,
+//             "phone_number": phone_number,
+//             "country_code": country_code
+//         },
+//         callback
+//     );
+// };
 
   
 
@@ -169,6 +201,6 @@ router.post('/s_checkOTP', function (req, res) {
 
 //   });
 
-})
+// })
 
 module.exports = router;
